44503f607e8da31dbba9b89c01a77a759032bea9
yashjariwala01 <jariwala.yash02@gmail.com>
2024-02-23 21:22:16 +0530

    version 1.01

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
                 <div className="flex justify-center items-center">
                     <div className="flex space-x-4 px-3 py-2 rounded-full">
                         <NavLink
-                            to="/"
+                            to="/home"
                             className="flex items-center gap-1 text-white text-lg font-semibold bg-gradient-to-r from-blue-500 to-purple-600 px-4 py-2 rounded-full"
                             activeClassName="text-yellow-400">
                             <MdHomeFilled className='mt-0.5' />
